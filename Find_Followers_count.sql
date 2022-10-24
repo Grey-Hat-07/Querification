449db0178dab069e1ea338b40b3030ae368bdcde
grey-hat-07 <subhamaynath2002@gmail.com>
2022-10-24 13:01:15 +0530

    commit

---
+++ Find_Followers_count.sql
@@ -0,0 +1,6 @@
+/*1729. Find Followers Count*/
+/* Write your PL/SQL query statement below */
+SELECT USER_ID,COUNT(FOLLOWER_ID) AS FOLLOWERS_COUNT
+FROM FOLLOWERS 
+GROUP BY USER_ID
+ORDER BY USER_ID
