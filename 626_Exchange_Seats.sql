6437ba3c6ab537a303d7adc0dbcb80ef742e9621
Diya <99545441+Diya032@users.noreply.github.com>
2023-10-30 11:54:50 +0530

    Update 626_Exchange_Seats.sql

--- 626_Exchange_Seats.sql
+++ 626_Exchange_Seats.sql
@@ -1,10 +1,10 @@
-#Method 1: 
-# select
-# if(id < (select count(*) from seat), if(id mod 2=0, id-1, id+1), if(id mod 2=0, id-1, id)) as id, student
-# from seat
-# order by id asc
+-- Method 1: 
+--  select
+--  if(id < (select count(*) from seat), if(id mod 2=0, id-1, id+1), if(id mod 2=0, id-1, id)) as id, student
+--  from seat
+--  order by id asc
 
-#Method 2:
+-- Method 2:
 select  
       case 
         when id % 2 = 0 then id - 1
